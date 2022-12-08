543fda4200f6abf56bda2f12c63952451719908b
Artem Kuzmenko <akuzmenko@codifi.me>
2022-12-08 12:44:54 +0300

    Second product added

--- Lesson 1/main.js
+++ Lesson 1/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const productName = "Playstation 5";
-const productQuantity = 10;
-const productCategory = "Games";
-const productPrice = 400;
+{
+    const productName = "Playstation 5";
+    const productQuantity = 10;
+    const productCategory = "Games";
+    const productPrice = 400;
+    
+    console.log('Product name: ', productName);
+    console.log('Products overall price: ', productQuantity * productPrice);
+}
 
-console.log('Product name: ', productName);
-console.log('Products overall price: ', productQuantity * productPrice);
+{
+    const productName = "TV Sony";
+    const productQuantity = 12;
+    const productCategory = "Electronics";
+    const productPrice = 700;
+    
+    console.log('Product name: ', productName);
+}
+
 
