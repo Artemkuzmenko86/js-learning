13e7e31aef5a58f7d674649545a5808221d9c2bc
Artem Kuzmenko <akuzmenko@codifi.me>
2022-12-08 12:39:50 +0300

    first commit

---
+++ Lesson 1/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const productName = "Playstation 5";
+const productQuantity = 10;
+const productCategory = "Games";
+const productPrice = 400;
+
+console.log('Product name: ', productName);
+console.log('Products overall price: ', productQuantity * productPrice);
+
